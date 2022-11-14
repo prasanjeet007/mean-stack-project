4d7a852b5f4cb031c4de6fea653e044e0d6feff2
Prasanjeet chatterjee <prasanjeetchatterjee@Prasanjeets-MacBook-Pro.local>
2022-11-15 00:32:45 +0530

    added-intial-routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/post-list', pathMatch:'full'},
+  {path:'post-create', component: PostCreateComponent},
+  {path:'post-list', component: PostListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
